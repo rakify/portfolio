0d89cd14870d382513e9fbf5f2e5924b617893f9
rakify <irakibm@gmail.com>
2022-12-20 01:00:19 +0600

    starting push, working on homepage

---
+++ components/Topbar.js
@@ -0,0 +1,46 @@
+import {
+  AppBar,
+  CssBaseline,
+  Divider,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { Box, Stack } from "@mui/system";
+import Link from "next/link";
+import React from "react";
+
+const Topbar = () => {
+  return (
+    <>
+      <CssBaseline />
+      <AppBar sx={{ backgroundColor: "#E3F2FD", color: "black" }}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <Typography variant="h6" component="div">
+            Rakib Miah
+          </Typography>
+          <Stack direction="row" alignItems="center" gap={1}>
+            <Link href="/">Home</Link>
+            <Divider orientation="vertical" flexItem />
+            <Link href="/about/">About</Link>
+            <Divider orientation="vertical" flexItem />
+            <Link href="/skills/">Skills</Link>
+            <Divider orientation="vertical" flexItem />
+            <Link href="/portfolio/">Portfolio</Link>
+            <Divider orientation="vertical" flexItem />
+            <Link href="/contact/">Contact</Link>
+          </Stack>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+    </>
+  );
+};
+
+export default Topbar;
